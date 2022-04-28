ca0001f83d761767afd9b6bc5f95a80d3941909c
Pavel Cerlat <64590513+PacliKilla@users.noreply.github.com>
2022-04-28 13:27:55 +0300

    Add files via upload

---
+++ Assets/Spaceship.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    int delay = 0;
+    GameObject a,b,c;
+    public GameObject bullet, explosion;
+    Rigidbody2D rb;
+
+    public float speed;
+    int health = 3;
+    int core1 = 0;
+    int core2 = 0;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        a = transform.Find("a").gameObject;
+        b = transform.Find("b").gameObject;
+        c = transform.Find("c").gameObject;
+    }
+    
+    void Start()
+    {
+        PlayerPrefs.SetInt("Health", health);
+    }
+
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0,Input.GetAxis("Vertical") * speed));
+ 
+
+        if (Input.GetKey(KeyCode.Space) && delay > 20)
+            Shoot();
+
+        delay++;
+    }
+
+    public void Damage()
+    {
+        health--;
+        PlayerPrefs.SetInt("Health", health);
+        StartCoroutine(Blink());
+        if (health == 0)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject,0.1f);
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+    }
+
+    void Shoot()
+    {
+        delay = 0;
+     
+        Instantiate(bullet, a.transform.position , Quaternion.identity);
+        Instantiate(bullet, b.transform.position , Quaternion.identity);
+
+        if (core1 > 3 && core2 > 3)
+            Instantiate(bullet, c.transform.position, Quaternion.identity);
+
+
+    }
+
+    public void AddHealth()
+    {
+        health++;
+        PlayerPrefs.SetInt("Health", health);
+    }
+
+    public void AddCore1()
+    {
+        core1++;
+        PlayerPrefs.SetInt("core1", core1);
+    }
+
+    public void AddCore2()
+    {
+        core2++;
+        PlayerPrefs.SetInt("core2", core2);
+    }
+
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    public float rate;
+    public GameObject[] enemies;
+    public int waves = 1;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", rate, rate);
+    }
+
+
+    void SpawnEnemy()
+    {
+        for(int i=0; i<waves; i++)
+            Instantiate(enemies[(int)Random.Range(0, enemies.Length)], new Vector3(Random.Range(-3.5f, 3.3f), 3, 0), Quaternion.identity);
+    }
+}
